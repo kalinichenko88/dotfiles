139778d99b504ef0906bb735a881538b0ccc7e45
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-01-29 22:26:16 +0400

    add initial wezterm configuration file

---
+++ wezterm.lua
@@ -0,0 +1,59 @@
+local wezterm = require 'wezterm'
+
+local function scheme_for_appearance(appearance)
+    if appearance:find('Dark') then
+        return 'OneDark (base16)'
+    else
+        return 'One Light (base16)'
+    end
+end
+
+local config = wezterm.config_builder()
+
+-- Rendering
+config.front_end = 'WebGpu'
+config.max_fps = 120
+
+-- Font
+config.font = wezterm.font_with_fallback {
+    { family = 'JetBrains Mono NL', weight = 'Medium' },
+    'Menlo',
+}
+config.font_size = 15.0
+config.line_height = 1.1
+config.freetype_load_flags = 'NO_HINTING'
+
+-- Colors
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+-- UI
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.window_padding = { left = 10, right = 10, top = 40, bottom = 8 }
+config.window_frame = {
+    font_size = 16.0,
+}
+
+-- Cursor
+config.default_cursor_style = 'BlinkingBar'
+config.cursor_blink_rate = 600
+
+-- Behavior
+config.scrollback_lines = 10000
+
+-- macOS
+config.native_macos_fullscreen_mode = true
+
+-- Dynamic color scheme switching
+wezterm.on('window-config-reloaded', function(window)
+    local overrides = window:get_config_overrides() or {}
+    local appearance = window:get_appearance()
+    local scheme = scheme_for_appearance(appearance)
+    if overrides.color_scheme ~= scheme then
+        overrides.color_scheme = scheme
+        window:set_config_overrides(overrides)
+    end
+end)
+
+return config
