133a421f039318fb133250ce6a658e1dc26a265d
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-02 23:10:04 +0400

    feat(diffview): add diffview.nvim plugin configuration

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+  'sindrets/diffview.nvim',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
+  --[[ keys = { ]]
+  --[[ { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diffview open' }, ]]
+  --[[ { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File history' }, ]]
+  --[[ { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = 'Branch history' }, ]]
+  --[[ { '<leader>gc', '<cmd>DiffviewClose<cr>', desc = 'Diffview close' }, ]]
+  --[[ }, ]]
+}
