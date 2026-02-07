abc90f8b6a21181a0840d82fffa21614708f1780
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-07 12:58:53 +0400

    feat(autopairs): add nvim-cmp as dependency

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,6 +1,9 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+  },
   config = function()
     local autopairs = require('nvim-autopairs')
     autopairs.setup({
