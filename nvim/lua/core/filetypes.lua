65ed6df0c93bb59c0627e6a8e87e76194052ec83
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-07 19:38:59 +0400

    feat(filetypes): add gitcommit filetype for git message files

--- nvim/lua/core/filetypes.lua
+++ nvim/lua/core/filetypes.lua
@@ -2,6 +2,10 @@ vim.filetype.add({
   filename = {
     ['.gitconfig'] = 'gitconfig',
     ['Brewfile'] = 'ruby',
+    ['COMMIT_EDITMSG'] = 'gitcommit',
+    ['MERGE_MSG'] = 'gitcommit',
+    ['TAG_EDITMSG'] = 'gitcommit',
+    ['SQUASH_MSG'] = 'gitcommit',
   },
   pattern = {
     ['gitconfig.*'] = 'gitconfig',
