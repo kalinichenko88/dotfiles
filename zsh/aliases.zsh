ebce96cb1b8d20a2efaac90f2d234be1d7e89db1
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-20 22:03:51 +0400

    feat(zsh): add common git, neovim, and shell aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,19 @@
 # Custom aliases
+
+# Git
+alias gs='git status'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gl='git log --oneline -20'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+
+# Neovim
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+
+# Shell
+alias reload='source ~/.zshrc'
