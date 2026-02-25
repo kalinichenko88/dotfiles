eda446ee622f132b45aa6fc2b6766433a06fbefd
Ivan Kalinichenko <ivan@kalinichenko.dev>
2026-02-25 23:24:39 +0400

    feat(autocommands): enable word wrap for markdown and text filetypes

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,3 +1,14 @@
+-- Enable word wrap for prose filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Enable word wrap for markdown and text files',
+  group = vim.api.nvim_create_augroup('prose-wrap', { clear = true }),
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
